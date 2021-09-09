1829259ceff4812e1f4fb8afbc0bf668a153ba47
Harinadh428 <harinadh01992@gmail.com>
2021-09-09 21:14:18 +0530

    harinadh_two_dimensional_matrix_multiplication

---
+++ src/main/arrays/twodimesionalarray/MatrixMultiplication.java
@@ -0,0 +1,53 @@
+package main.arrays.twodimesionalarray;
+
+/**
+ * @author harinadh dasari
+ */
+public class MatrixMultiplication {
+
+    public static void main(String[] args) {
+        int[][] matrix1 = {{2, 2, 2}, {2, 2, 2}};
+        int[][] matrix2 = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+
+        System.out.println("Displaying Matrix 1");
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[i].length; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Displaying Matrix 2");
+
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Matrix Multiplication Result");
+
+        int[][] product = new int[matrix1.length][matrix2[0].length];
+
+        for (int i = 0; i < matrix1.length; i++) {
+
+        }
+
+        for (int i = 0; i < product.length; i++) {
+            for (int j = 0; j < product[i].length; j++) {
+                for (int k = 0; k < matrix1[i].length; k++) {
+                    product[i][j] = product[i][j] + matrix1[i][k] * matrix2[k][i];
+                }
+            }
+        }
+
+
+        for (int i = 0; i < product.length; i++) {
+            for (int j = 0; j < product[i].length; j++) {
+                System.out.print(product[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
