201f88fea723561666dd8088af43a486b486ff2a
Harinadh428 <harinadh01992@gmail.com>
2021-09-19 19:42:39 +0530

    harinadh-binary-search-condition

--- src/main/arrays/interviewproblems/BinarySearch.java
+++ src/main/arrays/interviewproblems/BinarySearch.java
@@ -7,26 +7,30 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 41, 43, 50, 65};
-        int targetElement = 41;
+        int targetElement = 40;
         searchTheKey(arr, targetElement, 0, arr.length - 1);
     }
 
     private static void searchTheKey(int[] arr, int targetElement, int startIndex, int endIndex) {
 
-        if (startIndex < endIndex) {
-            int midIndex = (startIndex + endIndex) / 2;
+        if (arr.length > 2 && startIndex > endIndex) { // why we need this condition startIdx>endIndex because some times what will happen if value dosen't exist
+            // then we will keep on decreasing the endidx some point it will go negetive even though out start idx is 0
+            // but in general end idx will never go to -1
+            // that's why we have added this condition
+            System.out.println(-1);
+        }
+        int midIndex = (startIndex + endIndex) / 2;
 
-            if (targetElement == arr[midIndex]) {
-                System.out.println("Item Exist at Index :" + midIndex);
-                return;
-            }
+        if (targetElement == arr[midIndex]) {
+            System.out.println("Item Exist at Index :" + midIndex);
+            return;
+        }
 
-            if (targetElement < arr[midIndex]) {
-                searchTheKey(arr, targetElement, startIndex, midIndex - 1);
-            }
-            if (targetElement > arr[midIndex]) {
-                searchTheKey(arr, targetElement, midIndex+1, endIndex);
-            }
+        if (targetElement < arr[midIndex]) {
+            searchTheKey(arr, targetElement, startIndex, midIndex - 1);
+        }
+        if (targetElement > arr[midIndex]) {
+            searchTheKey(arr, targetElement, midIndex + 1, endIndex);
         }
     }
 }
