374a00a01d57c6593979e67d83e40eb088341d6a
Harinadh428 <harinadh01992@gmail.com>
2021-10-08 00:11:13 +0530

    harinadh-infix-evaluation-problem

---
+++ src/main/stack/interviewproblems/InfixEvaluationProblem.java
@@ -0,0 +1,83 @@
+package main.stack.interviewproblems;
+
+import java.util.Objects;
+import java.util.Stack;
+
+/**
+ * @author harinadh dasari
+ */
+public class InfixEvaluationProblem {
+
+    public static void main(String[] args) {
+
+        String str = "2+1+(3*2/2)+9";
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                operators.push(ch);
+            } else if (Character.isDigit(ch)) {
+                operands.push(ch - '0');
+            } else if (ch == ')') {
+                while (operators.size() > 0 && operators.peek() != '(') {
+                    char operator = operators.pop();
+                    int val1 = operands.pop();
+                    int val2 = operands.pop();
+
+                    int result = operation(val1, val2, operator);
+                    operands.push(result);
+                }
+                operators.pop();
+
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (operators.size() > 0 && operators.peek() != '(' && precedence(ch) <= precedence(operators.peek())) {
+
+                    char operator = operators.pop();
+                    int val1 = operands.pop();
+                    int val2 = operands.pop();
+                    int result = operation(val1, val2, operator);
+                    operands.push(result);
+                }
+                operators.push(ch);
+            }
+        }
+        while (operators.size() != 0) {
+            char operator = operators.pop();
+            int val1 = operands.pop();
+            int val2 = operands.pop();
+
+            int result = operation(val1, val2, operator);
+            operands.push(result);
+        }
+
+        System.out.println(operands.peek());
+    }
+
+    public static int operation(int val1, int val2, char operator) {
+
+        if (operator == '-') {
+            return val2 - val1;
+        } else if (operator == '+') {
+            return val2 + val1;
+        } else if (operator == '*') {
+            return val2 * val1;
+        } else {
+            return val1 / val2;
+        }
+    }
+
+    public static int precedence(char operator) {
+
+        if (Objects.equals(operator, '+')) {
+            return 1;
+        } else if (Objects.equals(operator, '-')) {
+            return 1;
+        } else if (Objects.equals(operator, '*')) {
+            return 2;
+        } else {
+            return 2;
+        }
+    }
+}
